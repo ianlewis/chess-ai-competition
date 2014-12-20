b671c6f3e46a7639439adea543c79004b4492e1b
Ian Lewis <IanMLewis@gmail.com>
2014-12-20 22:37:16 +0900

    Upadted data server
    * Added gorilla/mux as a dependency for URL parsing.
    * Added a game list api.
    * Exported fields so error responses are displayed properly.

--- dataapi/main.go
+++ dataapi/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
+    "strconv"
     "net/http"
     "encoding/json"
+    "github.com/gorilla/mux"
     // TODO: Use SQL driver.
     //_ "github.com/lib/pq"
     //"database/sql"
@@ -19,18 +21,18 @@ import (
 // JSON errors based on the Google style guide
 // https://google-styleguide.googlecode.com/svn/trunk/jsoncstyleguide.xml#Reserved_Property_Names_in_the_error_object
 type Error struct {
-    domain string
-    message string
-    location string
-    locationType string
-    extendedHelp string
-    sendReport string
+    Domain string `json:"domain"`
+    Message string `json:"message"`
+    Location string `json:"location"`
+    LocationType string `json:"locationType"`
+    ExtendedHelp string `json:"extendedHelp"`
+    SendReport string `json:"sendReport"`
 }
 
 type Errors struct {
-    code int64
-    message string
-    errors []Error
+    Code int64 `json:"code"`
+    Message string `json:"message"`
+    Errors []Error `json:"errors"`
 }
 
 var httpErrors = map[int64]string{
@@ -39,8 +41,10 @@ var httpErrors = map[int64]string{
     500: "Internal Server Error",
 }
 
+/*
+ * Return a json error message for the given code.
+ */
 func httpError(w http.ResponseWriter, code int64, err []Error) {
-    // Return a json error message.
     errors := Errors{
         code,
         httpErrors[code],
@@ -54,15 +58,60 @@ func httpError(w http.ResponseWriter, code int64, err []Error) {
     return
 }
 
+func asErrorList(err error) []Error {
+    return  []Error{
+        Error{
+            "Data",
+            err.Error(),
+            "",
+            "",
+            "",
+            "",
+        },
+    }
+}
+
 type Game struct {
     Id int64
     WhitePlayer int64
     BlackPlayer int64
-    IsPlaying bool
     Result int64
 }
 
-func getGame(w http.ResponseWriter, r *http.Request) {
+func getGames(writer http.ResponseWriter, request *http.Request) {
+    games := []Game{
+        Game{
+            123,
+            10,
+            11,
+            -1,
+        },
+        Game{
+            124,
+            10,
+            11,
+            -1,
+        },
+    }
+
+    bytedata, e := json.Marshal(games)
+    if (e != nil) {
+        httpError(writer, 500, asErrorList(e))
+        return
+    }
+
+    // Send the response.
+    fmt.Fprintf(writer, string(bytedata))
+}
+
+func getGame(writer http.ResponseWriter, request *http.Request) {
+    vars := mux.Vars(request)
+    gameId, idErr := strconv.ParseInt(vars["id"], 10, 64)
+    if (idErr != nil) {
+        httpError(writer, 404, asErrorList(idErr))
+        return
+    }
+
     // TODO: Use database.
     /*db, dbErr := openDB()
     if (dbErr != nil) {
@@ -81,33 +130,27 @@ func getGame(w http.ResponseWriter, r *http.Request) {
 
     // TODO: Get data from database.
     game := Game{
-        123,
+        gameId,
         10,
         11,
-        true,
         -1,
     }
 
     bytedata, e := json.Marshal(game)
     if (e != nil) {
-        httpError(w, 500, []Error{
-            Error{
-                "Data",
-                "Fatal json.Marshall() error.",
-                e.Error(),
-                "",
-                "",
-                "",
-            },
-        })
+        httpError(writer, 500, asErrorList(e))
         return
     }
 
     // Send the response.
-    fmt.Fprintf(w, string(bytedata))
+    fmt.Fprintf(writer, string(bytedata))
 }
 
 func main() {
-    http.HandleFunc("/", getGame)
+    router := mux.NewRouter()
+    router.HandleFunc("/game/{id}/", getGame)
+    router.HandleFunc("/game/", getGames)
+
+    http.Handle("/", router)
     http.ListenAndServe(":8080", nil)
 }
